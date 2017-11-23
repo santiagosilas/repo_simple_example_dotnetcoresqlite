6b25c4f5f491b6a8188d97f916bd05aa7aa54a4e
S. Santiago <santiagosilas@outlook.com>
2017-11-23 11:05:17 -0300

    a simple .net core console project with sqlite

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Exemplo baseado no tutorial 
+        /// disponível em https://developersoapbox.com/connecting-to-a-sqlite-database-using-net-core/
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var connStringBuilder = new SqliteConnectionStringBuilder();
+            connStringBuilder.DataSource = "./MyConsoleApp.db";
+            using (var connection = new SqliteConnection(connStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var cmdDeletarTabela = connection.CreateCommand();
+                cmdDeletarTabela.CommandText = "DROP TABLE IF EXISTS tarefas";
+                cmdDeletarTabela.ExecuteNonQuery();
+
+                var cmdCriarTabela = connection.CreateCommand();
+                cmdCriarTabela.CommandText = "CREATE TABLE tarefas(nome VARCHAR(50))";
+                cmdCriarTabela.ExecuteNonQuery();
+                
+                using (var transaction = connection.BeginTransaction())
+                {
+                    var cmdInserir = connection.CreateCommand();
+
+                    cmdInserir.CommandText = "INSERT INTO tarefas VALUES('Estudar C#.Net')";
+                    cmdInserir.ExecuteNonQuery();
+
+                    cmdInserir.CommandText = "INSERT INTO tarefas VALUES('Estudar .Net Core')";
+                    cmdInserir.ExecuteNonQuery();
+
+                    cmdInserir.CommandText = "INSERT INTO tarefas VALUES('Estudar Python 3')";
+                    cmdInserir.ExecuteNonQuery();
+
+                    transaction.Commit();
+                }
+
+                
+                var cmdSelect = connection.CreateCommand();
+                cmdSelect.CommandText = "SELECT nome FROM tarefas";
+
+                using (var reader = cmdSelect.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var message = reader.GetString(0);
+                        Console.WriteLine(message);
+                    }
+                }
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # repo_simple_example_dotnetcoresqlite
+
 A simple example with .Net Core and Sqlite (just to learn)
+
+This simple project was created on 2017 Visual Studio Community Edition.
+
+It uses the .Net Core app 2.0 as target framework. It also uses the following references (installed with Nuget):
+
+- Microsoft.EntityFrameworkCore.Sqlite (2.0.1)
+
+- Microsoft.EntityFrameworkCore.Sqlite.Design (1.1.4)
+ 
