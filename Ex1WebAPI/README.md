e875a02a77293a4425bf321da274ffa8447e6c61
S. Santiago <santiagosilas@outlook.com>
2017-12-12 10:19:35 -0300

    Defines an in-memory database, which is injected into the service container (services.AddDbContext)

--- Ex1WebAPI/README.md
+++ Ex1WebAPI/README.md
@@ -7,4 +7,7 @@ Step 1: Create the project
 Step 2: Create the Model Class Product (Id, Name, IsAvailable) in Models Folder
 
 Step 3: Create the database context
+
+Step 4: Register the database context
+	* The database context is registred with dependency injection
 	
--- Ex1WebAPI/Startup.cs
+++ Ex1WebAPI/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using Microsoft.EntityFrameworkCore;
+using Ex1WebAPI.Models;
+
 namespace Ex1WebAPI
 {
     public class Startup
@@ -23,6 +26,9 @@ namespace Ex1WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Here, we register the DB context (using the built-in support to dependency injection)
+            // Register a given context as a service in the IServiceCollection
+            services.AddDbContext<StoreContext>(opt => opt.UseInMemoryDatabase("ProductList"));
             services.AddMvc();
         }
 
