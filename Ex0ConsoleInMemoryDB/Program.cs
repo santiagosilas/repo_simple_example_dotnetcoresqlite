cc22e4681003dbc0b064c5a48bf488736757c81e
S. Santiago <santiagosilas@outlook.com>
2017-12-12 16:56:30 -0300

    .

---
+++ Ex0ConsoleInMemoryDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex0ConsoleInMemoryDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
